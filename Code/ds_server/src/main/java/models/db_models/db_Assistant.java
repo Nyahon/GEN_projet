3388652dc55b8b1ea5376041e0357b8e4291f9fb
zutt <yannlederrey@gmail.com>
2018-05-07 12:54:50 +0200

    modif base de données + code, ajout professeur from db

---
+++ Code/ds_server/src/main/java/models/db_models/db_Assistant.java
@@ -0,0 +1,7 @@
+package models.db_models;
+
+public class db_Assistant  extends db_Professeur {
+    public db_Assistant(int id, String nom, int pv, int niveau) {
+        super(id, nom, pv, niveau);
+    }
+}
